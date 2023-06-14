d6990f0d623a86ec35b07970f29ed5791ebceb01
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-06-14 18:32:44 +0700

    fix spite mushroom goal

--- MarioBros3/Coin.h
+++ MarioBros3/Coin.h
@@ -24,7 +24,6 @@ public:
 		isInQuestionBlock = k;
 	}
 	void Render();
-	//void Update(DWORD dt);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
